7159da03ee6216ebc4d27006fdadf634b44b44a6
Sheng Yunzhou <shengyunzhou@gmail.com>
2018-10-13 09:51:20 +0800

    Add game plan and basic rendering of carpark availability data

---
+++ GAMEPLAN.md
@@ -0,0 +1,31 @@
+# Game Plan for Training
+
+## Explain HTML, CSS, JavaScript
+- Explain where frontend development sits in the entire software development world
+- Show some live example
+- Provide some useful links
+
+## Explain Chrome developer tool
+- Show different tabs
+- Show how console can be useful
+
+## Make some static changes to Create React App
+- Change the h1 title
+- Add some button
+- Add an event handler
+
+## Make an async call
+- Install `axios` from `npm`
+- Show how `package.json` is changed
+- Explain why this is important
+- Touch a bit on how to do this professionally - e.g. Docker
+
+## Use JSX to display data on screen
+- Show how to use `map` to display an array of data into HTML elements
+- Discuss at this stage the current code base and where it can be improved
+
+## Add logic to manipuate the data
+- Take the first 10 carparks 
+- Sort the carparks by availability
+- Filter the carparks by type
+- Use TDD to drive this
--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
 import React, { Component } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      availability: null
+    };
+    this.loadData = this.loadData.bind(this);
+  }
+  loadData() {
+    // Discuss in class with Max the problem with this code
+    // E.g. no null check, mixing http logic with view, magical "this" binding
+    // Maybe even introduce lifecycle events here, e.g. componentDidMount
     axios
       .get("https://api.data.gov.sg/v1/transport/carpark-availability")
-      .then(console.log);
+      .then(response => {
+        const availability = response.data.items[0].carpark_data;
+        console.log(availability);
+        this.setState({ availability });
+      });
   }
   render() {
+    // Explain the `map` call below, make sure students understand the power of JSX
+    // Explain why there needs to be a `key` prop in `map`
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Singapore HDB Carpark Availability</h1>
+        <button onClick={this.loadData}>Click me to load data</button>
+        {this.state.availability ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Carpark Number</th>
+                <th>Total Lots</th>
+                <th>Lot Type</th>
+                <th>Lots Available</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.availability.map(availability => (
+                <tr key={`${availability.carpark_number}-${availability.carpark_info[0].lot_type}`}>
+                  <td>{availability.carpark_number}</td>
+                  <td>{availability.carpark_info[0].total_lots}</td>
+                  <td>{availability.carpark_info[0].lot_type}</td>
+                  <td>{availability.carpark_info[0].lots_available}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : null}
       </div>
     );
   }
